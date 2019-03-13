71b70256b0fda921f59845733ae2794e4efd385f
JayWooKJ <natcho9010@gmail.com>
2019-03-13 19:04:07 +0900

    HomeController 원래대로 돌립니다.

--- src/main/java/kr/gangwon/wonju/HomeController.java
+++ src/main/java/kr/gangwon/wonju/HomeController.java
@@ -10,8 +10,6 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		
-		System.out.println("asdasdasd");
-		///asfasfefawef
 		
 		return "index";
 	}
